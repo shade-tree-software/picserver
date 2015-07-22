0ded7d950d463a62a7fc188afbd25691aa92c2fa
andrewhamilton <andrew.hamilton@exelisinc.com>
2015-07-22 09:11:39 -0400

    added image upload

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+
+  def upload
+    uploaded_io = params[:picture]
+    dir = Rails.root.join('public', 'uploads')
+    Dir.mkdir(dir) unless Dir.exists?(dir)
+    tempfile = File.new File.join(dir, uploaded_io.original_filename), 'w', :encoding => 'binary'
+    tempfile.write uploaded_io.read
+    tempfile.flush
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  match ':controller/:action', via: [:get, :post]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

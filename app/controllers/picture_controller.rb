fe71bc2a7215c228057e59e7db26f53a3ec5a76c
andrewhamilton <andrew.hamilton@exelisinc.com>
2015-07-26 12:51:28 -0400

    pictures now display properly

--- app/controllers/picture_controller.rb
+++ app/controllers/picture_controller.rb
@@ -12,6 +12,7 @@ class PictureController < ApplicationController
     Dir.mkdir(dir) unless Dir.exists?(dir)
     uid = SecureRandom.urlsafe_base64
     filename = File.join dir, "#{uid}#{File.extname uploaded_io.original_filename}"
+    puts "Writing #{filename}"
     tempfile = File.new filename, 'w', :encoding => 'binary'
     tempfile.write uploaded_io.read
     tempfile.flush
@@ -20,7 +21,15 @@ class PictureController < ApplicationController
   end
 
   def show
-    @pic = Picture.find_by_uid(params[:id])
+    @picture = Picture.find_by_uid(params[:id])
+  end
+
+  def raw
+    @picture = Picture.find_by_uid(params[:id])
+    dir = Rails.root.join('public', 'uploads')
+    filename = File.join dir, "#{@picture.uid}#{File.extname(@picture.original_filename)}"
+    image_data = File.read filename
+    send_data image_data
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :picture
+  resources :picture do
+    member do
+      get 'raw'
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

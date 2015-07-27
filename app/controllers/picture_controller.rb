6a58828286625f089096478268cf728510aa87ef
andrewhamilton <andrew.hamilton@exelisinc.com>
2015-07-27 16:13:29 -0400

    moved picture storage to database

--- app/controllers/picture_controller.rb
+++ app/controllers/picture_controller.rb
@@ -3,38 +3,23 @@ require 'securerandom'
 class PictureController < ApplicationController
 
   def new
-
   end
 
   def create
-    uploaded_io = params[:picture]
-    dir = File.join 'public', 'uploads'
-    Dir.mkdir(dir) unless Dir.exists?(dir)
-    uid = SecureRandom.urlsafe_base64
-    filename = File.join dir, "#{uid}#{File.extname uploaded_io.original_filename}"
-    puts "Writing #{filename}"
-    tempfile = File.new filename, 'w', :encoding => 'binary'
-    tempfile.write uploaded_io.read
-    tempfile.flush
-    @pic = Picture.create(original_filename: uploaded_io.original_filename,
-                          uid: uid)
+    @pic = Picture.create(original_filename: params[:picture].original_filename,
+                          uid: SecureRandom.urlsafe_base64,
+                          data: params[:picture].read)
   end
 
   def show
     @picture = Picture.find_by_uid(params[:id])
   end
 
-  def get_filename_by_uid(uid)
-    picture = Picture.find_by_uid(uid)
-    File.join 'public', 'uploads', "#{picture.uid}#{File.extname(picture.original_filename)}"
-  end
-
   def raw
-    send_data File.read(get_filename_by_uid params[:id])
+    send_data Picture.find_by_uid(params[:id]).data
   end
 
   def delete
-    File.delete(get_filename_by_uid params[:id])
     Picture.find_by_uid(params[:id]).destroy
   end
 

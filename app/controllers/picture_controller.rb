c371d13773358fc5aa7e53db30cef007cd311ad0
andrewhamilton <andrew.hamilton@exelisinc.com>
2015-12-09 15:42:33 -0500

    added default content type

--- app/controllers/picture_controller.rb
+++ app/controllers/picture_controller.rb
@@ -24,7 +24,7 @@ class PictureController < ApplicationController
         # pic exists, is viewable, and has no expiry date.  show pic once and never again.
         pic.viewable = false
         pic.save
-        send_data pic.data, :disposition => :inline, :type => pic.content_type
+        send_data pic.data, :disposition => :inline, :type => (pic.content_type || 'application/octet-stream')
       elsif pic.expiry <= Time.now.to_i
         # pic exists and is viewable, but expiry is in the past.  never show the pic.
         pic.viewable = false
@@ -32,7 +32,7 @@ class PictureController < ApplicationController
         nil
       else
         # pic exists and expiry date is in the future.  show pic and keep it viewable.
-        send_data pic.data, :disposition => :inline, :type => pic.content_type
+        send_data pic.data, :disposition => :inline, :type => (pic.content_type || 'application/octet-stream')
       end
     else
       # pic does not exist or is no longer viewable.
